842f06e60bbd63268f3cb7ac52544d9e18b5cc35
yaboisimo <anagaaco@gmail.com>
2021-10-24 01:40:02 +0100

    webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+require ('laravel-mix-tailwind');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,4 +16,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .tailwind();
